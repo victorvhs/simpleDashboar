617b9a248e3c3b89fdd6dbc7317867b707af2b8f
Victor Reis <victor.h.s.reis@gmail.com>
2021-11-18 23:41:17 -0300

    home

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+import 'animate.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ src/views/Home/CustomHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Header is here</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
